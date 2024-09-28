a39342659a9d469d30eb1e1b17745a365f276ee5
k0v0354 <k0v0354@homeoffice.wal-mart.com>
2024-09-28 20:20:01 +0530

    Initial Commit

---
+++ FitHabit/FitHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitHabitApp.swift
+//  FitHabit
+//
+//  Created by Keerthanaa Vm on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FitHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitHabitUITests/FitHabitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FitHabitUITestsLaunchTests.swift
+//  FitHabitUITests
+//
+//  Created by Keerthanaa Vm on 28/09/24.
+//
+
+import XCTest
+
+final class FitHabitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
